6c555cb63251e5063e82d1539feb310674fe385d
Alberto Obando <alberto.obando98@gmail.com>
2020-09-17 14:01:17 -0600

    Upvotes

--- AlbertoBlog/my-blog-backend/src/server.js
+++ AlbertoBlog/my-blog-backend/src/server.js
@@ -1,16 +1,32 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+//Fake data base
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+    },
+    'learn-node': {
+        upvotes: 0,
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+    },
+}
+
 //We use express as our server for the app
 const app = express();
 
 //Reads the contents from the body with JSON formats
 app.use(bodyParser.json());
 
-//Goes to /hello page for get methods
-app.get(`/hello`, (req, res) => res.send('Hello!'));
-//Goes to the /hello page for post methods
-app.post(`/hello`, (req, res) => res.send(`Hello ${req.body.name}!`));
+//Upvoting articles
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+});
 
 //Creates localhost that listens in port 8000
 app.listen(8000, () => console.log('Listeting on port 8000'));
